92e23693837001b3b85775f96c2d4f65b80f6534
Kareem H Mahmoud <92833648+Karccmmm@users.noreply.github.com>
2021-12-06 23:39:52 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,39 @@
+//recursive tower of hanoi program in c++
+//Kareem Mahmoud-COSC1437
+//Teacher: Dr_T
+
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void towerOfHanoi(int n, char from_rod,char to_rod, char aux_rod)
+{
+    if(n == 1)
+    {
+        cout <<"Move disk 1 from rod " << from_rod <<" to rod " << to_rod<<endl;
+        return;
+    }
+    towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+    cout << "Move disk " << n << " from rod " << from_rod <<" to rod " << to_rod << endl;
+    towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
+}
+ 
+int main()
+{
+    int n = 4; // Number of disks
+    towerOfHanoi(n, 'A', 'C', 'B'); // A, B and C are names of rods
+    return 0;
+}
+
+
+//About Tower of Hanoi :
+
+//*Tower of Hanoi is a mathematical puzzle where we have three rods and n disks. The objective of the puzzle is to move the entire stack to another rod, obeying the following simple rules:
+
+//1.Only one disk can be moved at a time.
+//2.Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack i.e. a disk can only be moved if it is the uppermost disk on a stack.
+//3.No disk may be placed on top of a smaller disk.
+
+//2. Recursion is a process in which a function calls itself as a subroutine. This allows the function to be repeated several times, since it calls itself during its execution.
+
+
